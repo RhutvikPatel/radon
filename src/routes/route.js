cbe9074a11bde691f9b2d5f207117030059092bc
Rhutvik Patel <rhutvikpatel007@gmail.com>
2022-06-03 21:51:00 +0530

    updated assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const underscore = require('underscore');
 const { chunk } = require('underscore');
 const _ = require('lodash');
+const { result } = require('lodash');
 
 const router = express.Router();
 
@@ -14,7 +15,7 @@ router.get('/hello', function (req, res) {
     let last9Elements = _.tail(first10OddNumbers)
     console.log(last9Elements)
 
-    let arr1=[1,2]; let arr2=[1,4,5]; let arr3=[12,2]; let arr4=[5, 6]; let arr5=[7, 6, 5, 2]; 
+    let arr1=[1,2]; let arr2=[1,4,5]; let arr3=[12,2]; let arr4=[5, 6]; let arr5=[7, 6, 5, 2];
     let arrayresult=_.union(arr1,arr2,arr3,arr4,arr5)
     console.log(arrayresult)
 
@@ -33,7 +34,83 @@ router.get('/hello', function (req, res) {
 
 router.get('/movies/:indexNumber', function(req, res){
     const moviesName=["X-Men", "Avengers", "Dr. Strange", "6 Underground", "Escape Room", "Top Gun"]
-    res.send(moviesName[req.params.indexNumber])
+    let len= moviesName.length
+
+    let checkIndex=0
+    for(let i in moviesName)
+    {
+        if(req.params.indexNumber>=len){
+            checkIndex=1
+            res.send("The index given is greater then lenght of array, please provide valid index")
+            break
+        }
+    }
+    if(checkIndex==0){
+        res.send(moviesName[req.params.indexNumber])
+    }
+})
+
+router.get('/films', function(req, res){
+    let movies=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+    res.send({movies})
+})
+
+router.get('/films/:filmId', function(req, res){
+    let movies=[ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo"
+       }]
+
+    
+    if(checkId==0){
+        let resultMovie
+        let id=req.params.filmId
+        let index=id-1
+        if(index<=movies.length){
+            res.send(movies[index])
+        }
+        else{
+            res.send("No movie exists with this id")
+        }
+        // let checkId=0
+        // for(let i in movies)
+        // {
+        //     if(req.params.filmId===movies.id){
+        //         checkId=1
+        //         res.send("No movie exists with this id")
+        //         break
+        //     }
+        // }
+        // for(let i in movies){
+        //     if(movies.id===id)
+        //     {
+        //        resultMovie=movies[i]
+        //         break
+        //     }
+        // }
+        // res.send( resultMovie )
+    }
 })
 
 // router.get('/candidates', function(req, res){
@@ -56,7 +133,7 @@ router.get('/movies/:indexNumber', function(req, res){
 
 router.get("/sol1", function (req, res) {
     //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
-    let arr= [1,2,3,5,6,7]   
+    let arr= [1,2,3,5,6,7]
     let sumOfTheArray=0
     for(let i in arr){
         sumOfTheArray += arr[i]
@@ -68,7 +145,7 @@ router.get("/sol1", function (req, res) {
     res.send(  { data: missingNumber  }  );
 });
 
-app.get("/sol2", function (req, res) {
+router.get("/sol2", function (req, res) {
     //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
     let arr= [33, 34, 35, 37, 38]
     let lenght=arr.length
@@ -76,7 +153,7 @@ app.get("/sol2", function (req, res) {
     for(let i in arr){
         sumOfTheArray +=arr[i]
     }
-    
+
     let firstElement= arr[0]
     let lastElement= arr.pop()
     let consecutiveSumOfArray = (lenght+ 1)*(firstElement + lastElement)/2
