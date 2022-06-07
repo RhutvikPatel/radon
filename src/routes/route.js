6cbc2465d993a4a8fb488e7e93dd638855bd2fda
RhutvikPatel <rhutvikpatel007@gmail.com>
2022-06-07 13:58:15 +0530

    Assignment of mongoDb-session2

--- src/routes/route.js
+++ src/routes/route.js
@@ -16,4 +16,12 @@ router.post("/createBook", BookController.createBook  )
 
 router.get("/getBooksData", BookController.getBooksData)
 
+router.post("/getBooksInYear", BookController.getBooksInYear)
+
+router.get("/getXINRBooks", BookController.getXINRBooks)
+
+router.get("/getRandomBooks", BookController.getRandomBooks)
+
+router.get("/getParticularBooks", BookController.getParticularBooks)
+
 module.exports = router;
